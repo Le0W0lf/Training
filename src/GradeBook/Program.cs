859da17673d47cff7edb5331c18922df6aa109cf
Viktor Svizhak <VSvizhak@lan.ad>
2019-05-08 15:08:49 +0300

    Add project files.

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GradeBook
+{
+    
+    class Program
+    {
+        int Coun=0;
+        static void Main(string[] args)
+        {
+            int s;
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+    }
+    
+
+}
